b19d3299360c550fb02ba64aaf17f2254b2a2915
npaudyal321 <naween321@gmail.com>
2024-01-01 13:22:48 +0545

    datatypes

---
+++ day2/1_syntax.py
@@ -0,0 +1,16 @@
+a = 1
+# 1a = 2
+
+# a@b = 3
+a_b = 3
+
+a3 = 4
+
+# def = 4
+
+# Indentation is very important in Python
+# We do not use curly braces to enclose a block of code in Python
+
+a = 5
+if a == 7:
+    print("The number is 7")
---
+++ day2/2_datatypes.py
@@ -0,0 +1,12 @@
+# There are broadly two types of datatypes in Python
+# 1. Mutable Datatypes
+# 2. Immutable Datatypes
+
+
+# 1. Mutable Datatypes: All those objects which can be modified after their creation are the
+# mutable objects/datatypes. E.g List, Dictionary, Set
+
+# 2. Immutable Datatypes: All those objects which can't be modified after their creation are the
+# immutable objects. E.g. String, Tuple, Numbers, Boolean.
+
+
